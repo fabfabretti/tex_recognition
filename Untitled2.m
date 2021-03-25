c63b0e413a74b27240b60e8fb10d5bad186d2b34
fabfabretti <fabretti.fabiola@gmail.com>
2021-03-25 21:23:19 +0100

    Final

--- Untitled2.m
+++
@@ -1,4 +0,0 @@
-pf=makedist('InverseGaussian','mu',1,'lambda',1);%establish whatever you want for mu and lambda
-x=0:.001:2;%establish whatever values you want evaluated
-y=pdf(pf,x);
-plot(x,y);
--- tex_analysis.m
+++ tex_analysis.m
@@ -1,51 +1,38 @@
-clear 
+clear
 close all
 clc
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% @ CESARE!! 
-% Relazione: 
-%    [TODO: PRENDERE PIù KERNEL VARIEGATI + MEDIA]
-%   comunque già così riconosce 34 immagini (ho migliorato alcune cose) ^-^
-%  
-    %  RELAZIONE:
-    %  https://docs.google.com/document/d/1oUnQE5UUykekRmstjFRi4RPNVbs6H3F_cKcTdAV1ZF0/edit?usp=sharing
-%
-%   Per mantenere questo script (dato che già va e abbiamo poco tempo)
-%   potremmo anche usare 1 sola dimensione kernel (aka quella che torna da
-%   find_pattern_size), limitarci a prendere N volte quella lì e fare
-%   media/somme/moltiplicazioni
-%   In particolare, se facciamo questa cosa dentro al for krn=1:2 (linea
-%   84), si fa la media usando una volta dim=CONT e una volta dim=CORR e si
-%   generano due mappe separate. A quel punto il controllo finale decide 
-%   quale usare (o se usare GABOR).
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+rng(1);
+
+
 %% --- IMPOSTAZIONI
 
 % ---- Scelta dell'input e gestione files
-    analyze_just_one = false; % Se true, analizza una sola immagine; 
-                             % altrimenti analizza tutta la cartella
+analyze_just_one = true; % Se true, analizza una sola immagine;
+% altrimenti analizza tutta la cartella
 
-    rand_image = false; % Se true e se analyze_just_one è true, sceglie 
-                        % randomicamente l'immagine da analizzare. 
-                        % Altrimenti sceglie la unrand_number-esima.
-                        
-    unrand_number = 2;  % Se rand_image è false, seleziona l'immagine.
-    
-    flush_folder=false; % Se true, svuota la cartella result prima 
-                        % di iniziare
-                        
+rand_image = false; % Se true e se analyze_just_one è true, sceglie
+% randomicamente l'immagine da analizzare.
+% Altrimenti sceglie la unrand_number-esima.
+
+unrand_number = 2;  % Se rand_image è false, seleziona l'immagine.
+
+flush_folder=false; % Se true, svuota la cartella result prima
+% di iniziare
+
 % ---- Impostazioni di risultato
-    show_resume = true;  % se true apre la figura di riassunto coi passaggi
-    
-    show_resume_choice = false; %considerto solo se show_resume è true; mostra 
-                              % sia la mappa CONT che la CORR.
-                              
-    show_result = false; % se true apre una figura che mostra la zona 
-                         % selezionata
+show_resume = true;  % se true apre la figura di riassunto coi passaggi
+
+show_resume_choice = false; %considerto solo se show_resume è true; mostra
+% sia la mappa CONT che la CORR.
+
+show_result = false; % se true apre una figura che mostra la zona
+% selezionata
+kernel_amount = 50;
 
 % ---- Parametri per l'analisi
-    disk_dim = 5;% Specifica la dimensione da usare per la open della maschera
-    areaopen = 300; % Specifica l'area minima selezionabile.
+disk_dim = 5;% Specifica la dimensione da usare per la open della maschera
+areaopen = 300; % Specifica l'area minima selezionabile.
 
 %% --- RUN
 
@@ -53,90 +40,144 @@ clc
 %% Caricamento files
 
 % ---- Gestione cartella risultati
-    if not(isfolder('results'))
-        mkdir('results')
-    end
+if not(isfolder('results'))
+    mkdir('results')
+end
 
-    if flush_folder == true
-        delete('results\*')
-    end
+if flush_folder == true
+    delete('results\*')
+end
 
 % ---- Caricamento tutte le immagini
-    files = dir('defect_images\*.jpg');
+files = dir('defect_images\*.jpg');
 
 % ---- Gestione di _quali_ immagini analizzare (secondo le impostazioni)
-    to_be_analyzed = length(files);
-    if analyze_just_one == true
-        to_be_analyzed = 1;
-    else
-        rand_image = false;
-    end
+to_be_analyzed = length(files);
+if analyze_just_one == true
+    to_be_analyzed = 1;
+else
+    rand_image = false;
+end
 
-    
-     kernel_types = ["CORR","CONT"]; %mi serve solo per avere le stringhe pronte comode
-    
+
+kernel_types = ["CORR","CONT"]; %mi serve solo per avere le stringhe pronte comode
+
 % for loop for each file in folder:
 for fn=1:to_be_analyzed
- i=fn;
+    i=fn;
     [IMG_RGB, filename]= fileloader(fn,files,analyze_just_one,rand_image,unrand_number);
     IMG = rgb2gray(IMG_RGB); % 512x512
     [IMG_x,IMG_y]=size(IMG);
     
-%% - Analisi
-
-% --- Ricerca dimensione ottimale dei kernels
-     
-     % Salva le due pattern size ideali trovate dalla funzione apposita,
-     % per provarle in sequenza.
-     [kernel_corr, kernel_cont]  = find_pattern_size(IMG);
-     kernels = [kernel_corr, kernel_cont];
-
+    %% - Analisi
     
-     % L'analisi base (xcorr) viene fatta due volte: una usando il kernel 
-     % che massimizza CORR e una usando quello che massimizza CONT. Poi si
-     % decide quale delle due maschere è più bella.
-    for krn=1:2   
+    % --- Ricerca dimensione ottimale dei kernels
+
+    % Salva le due pattern size ideali trovate dalla funzione apposita,
+    % per provarle in sequenza.
+    [kernel_corr, kernel_cont]  = find_pattern_size(IMG);
+    kernels = [kernel_corr, kernel_cont];
+    
+    
+    % L'analisi base (xcorr) viene fatta due volte: una usando il kernel
+    % che massimizza CORR e una usando quello che massimizza CONT. Poi si
+    % decide quale delle due maschere è più bella.
+    for k_a=1:2
         
-     kernel_dim = kernels(krn);
-     kernel_type = kernel_types(krn);
-     fprintf('A.%d) Kernel %s scelto : %d\n',krn,kernel_type,kernel_dim);
-
-    % ---- Definizione dei 6 kernels (in riga per brevità)
-        pattern1 = IMG(1:kernel_dim,1:kernel_dim);pattern2 = IMG(2:kernel_dim+1,2:kernel_dim+1); pattern3 = IMG(IMG_x-kernel_dim+1:IMG_x,IMG_y-kernel_dim+1:IMG_y);    pattern4 = IMG(IMG_x-kernel_dim:IMG_x-1,IMG_y-kernel_dim:IMG_y-1);pattern5 = IMG(1:kernel_dim,IMG_y-kernel_dim+1:IMG_y);    pattern6 = IMG(2:kernel_dim+1,IMG_y-kernel_dim+1:IMG_y);
-
-    % ---- Calcolo della xcorr. (in riga per brevità)
-        c1 = normxcorr2(pattern1,IMG);    c2 = normxcorr2(pattern2,IMG);    c3 = normxcorr2(pattern3,IMG);c4 = normxcorr2(pattern4,IMG);    c5 = normxcorr2(pattern5,IMG);    c6 = normxcorr2(pattern6,IMG);
-
-        xcorr_full = (c1+c2+c3+c4+c5+c6)/6; % calcolo media 
-
+        kernel_dim = kernels(k_a);
+        kernel_type = kernel_types(k_a);
+        fprintf('A.%d) Kernel %s scelto : %d\n',k_a,kernel_type,kernel_dim);
+        
+        % ---- Genero la xcorr "di base" dove farò la media di tutte le altre
+        xcorr_full = zeros(IMG_x + kernel_dim-1, IMG_y + kernel_dim-1);
+        
+        % Zona massima considerabile (non voglio pescare kernel fuori
+        % dalla zona)
+        pat_x_max = IMG_x - kernel_dim;
+        pat_y_max = IMG_y - kernel_dim;
+        
+        % genero il posto dove salverò le posizioni dei kernel
+        if k_a ==1
+            kern_pos_corr=cell(1,kernel_amount);
+        else
+            kern_pos_cont=cell(1,kernel_amount);
+        end
+        
+        for krn=1:kernel_amount
+            % Estraiamo a sorte le posizioni del pattern. Consideriamo
+            % solamente posizioni che si trovano negli angolini
+            % dell'immagine, perché spesso e volentieri il difetto è in
+            % mezzo
+            
+            while true
+                pat_x = randi(pat_x_max);
+                if pat_x < IMG_x/4 || pat_x > (IMG_x - IMG_x/4)
+                    break;
+                end
+            end
+            while true
+                pat_y = randi(pat_y_max);
+                if pat_y < IMG_y/4 || pat_y > (IMG_y - IMG_y/4)
+                    break;
+                end
+            end
+            
+            %Salviamo le coordinate estratte nelle celle rispettive
+            if k_a ==1
+                kern_pos_corr{krn}=[pat_x, pat_y];
+            else
+                kern_pos_cont{krn}=[pat_x, pat_y];
+            end
+            
+            % Estraiamo il pattern (nella posizione estratta) dall'immagine
+            pat = IMG( pat_x : (pat_x+kernel_dim-1) ,  pat_y : (pat_y+kernel_dim-1) );
+            
+            % Aggiungiamo la crosscorrelazione attuale alla media
+            xcorr_full = xcorr_full + 1/kernel_amount .* normxcorr2(pat,IMG);
+        end
+        
         % Tagliamo la xcorr alla dimensione corretta
-        xcorr = xcorr_full(kernel_dim-1:end-kernel_dim+1,kernel_dim-1:end-kernel_dim+1); % size(pattern)-1 
+        xcorr = xcorr_full(kernel_dim-1:end-kernel_dim+1,kernel_dim-1:end-kernel_dim+1); % size(pattern)-1
         xcorr = abs(xcorr);
-        xcorr = imgaussfilt(xcorr,1);
-
-    % ---- Calcoliamo la treshold ideale con Otsu 
-           T = graythresh(xcorr);
-
-    % ---- Generiamo la maschera
+        xcorr = imgaussfilt(xcorr,2);
+        imagesc(xcorr);
+        pause(.5)
+        
+        % ---- Calcoliamo la treshold ideale con Otsu
+        T = graythresh(xcorr(30:end-30, 30:end-30))*.9;
+        
+        % ---- Generiamo la maschera
         mask_raw = xcorr<T;
-
-    % ---- Refining della maschera
-        se = strel('disk',disk_dim); 
+        
+        % ---- Refining della maschera
+        se = strel('disk',disk_dim);
         mask = imopen(mask_raw,se);
         mask = imclose(mask,se);
         mask = bwareaopen(mask, areaopen);
         
         
         %Salviamo la maschera finale (nel tipo corretto)
-        if krn==1
+        if k_a==1
+            xcorr_corr = xcorr;
             mask_corr = mask;
+            mask_raw_corr = mask_raw;
         else
+            xcorr_cont = xcorr;
             mask_cont = mask;
-        end
-
+            mask_raw_cont = mask_raw;
+        end        
     end
     
-% ---- Scelta della maschera migliore:
+    
+        figure();
+        subplot(231);        imagesc(xcorr_corr);        title("xcorr corr");
+        subplot(232);        imshow(mask_raw_corr);        title("mask corr");
+        subplot(233);        imshow(mask_corr);        title("mask corr");
+        subplot(234);        imagesc(xcorr_cont);        title("xcorr cont");
+        subplot(235);        imshow(mask_raw_cont);        title("mask cont");
+        subplot(236);        imshow(mask_cont);        title("mask cont");
+    
+    % ---- Scelta della maschera migliore:
     % Di default prendo CORR.
     % Passo a CONT se:
     %   - CONT ha meno aree
@@ -145,27 +186,40 @@ for fn=1:to_be_analyzed
     
     [topology_corr, selected_ratio_corr] = is_reliable(mask_corr,IMG);
     [topology_cont, selected_ratio_cont] = is_reliable(mask_cont,IMG);
-
-    mask = mask_corr;
-
-    if selected_ratio_corr == 0 && selected_ratio_cont <= 0.1
-        mask= mask_cont;
-        kernel_type = 'CONT';
+    fprintf("[Corr: %d, %.1f%%\tCont: %d, %.1f%%]\n",...
+        topology_corr, selected_ratio_corr,topology_cont, selected_ratio_cont);
+    
+    choice = "corr";    
+    if selected_ratio_corr <= 0.1 
+        choice = "cont";
         fprintf("B) Switching to CONT because CORR mask was empty\n");
     elseif topology_cont < topology_corr
-        mask = mask_cont;
-        kernel_type = 'CONT';
+        choice = "cont";
         fprintf("B) Switching to CONT because of bad topology\n");
     elseif topology_cont == topology_corr && selected_ratio_cont > selected_ratio_corr
-        mask = mask_cont;
-        kernel_type = 'CONT';
+        choice = "cont";
         fprintf("B) Switching to CONT because of same topology + better selection ratio.\n");
     else
         fprintf("B) CORR map seems optimal.\n");
-        kernel_type = 'CORR';
-        kernel_dim = kernel_corr;
     end
-
+    
+    if choice == "corr"
+        xcorr = xcorr_corr;
+        kernel_dim = kernel_corr;
+        mask_raw = mask_raw_corr;
+        mask= mask_corr;
+        kernel_type = 'CORR';
+        kern_pos = kern_pos_corr;
+    else
+        xcorr = xcorr_cont;
+        kernel_dim = kernel_cont;
+        mask_raw = mask_raw_cont;
+        mask= mask_cont;
+        kernel_type = 'CONT';
+        kern_pos = kern_pos_cont;
+    end
+    
+    
     if selected_ratio_cont <= 0.1
         kernel_type = 'GABOR';
         fprintf("B.2) Both masks are empty; switching to GABOR. Potrebbe volerci qualche secondo.. >>\n");
@@ -173,125 +227,129 @@ for fn=1:to_be_analyzed
         kernel_dim = 1;
     end
     
-% ---- Ritaglio IMG (solo se NON sto usando Gabor) e applicazione maschera
+    % ---- Ritaglio IMG (solo se NON sto usando Gabor) e applicazione maschera
     if kernel_type ~= "GABOR"
         border = kernel_dim / 2;
         warning('off');IMG=IMG(border:end-border+1,border:end-border+1);warning('on');
     end
-
+    
     % Clippiamo al massimo i valori dell'immagine che corrispondono alla maschera
     IMG_selected = IMG;    IMG_selected(mask)=255;
-
+    
     % Creiamo immagine a tre canali mettendo la versione selezionata sulcanale rosso
-     IMG_masked=cat(3,IMG_selected,IMG,IMG);
-
+    IMG_masked=cat(3,IMG_selected,IMG,IMG);
     
-%% --- F I G U R E S 
-% Questa sezione genera (e salva) le immagini di risultato, secondo quello
-% che è settato dalle impostazioni.
-
-   [~, selected_pixels_ratio] = is_reliable(mask,IMG); % mi tornerà utile :)
-   
-if show_resume == true
     
-% --- Figure 1: riassuntazzo
-    figure();
+    %% --- F I G U R E S
+    % Questa sezione genera (e salva) le immagini di risultato, secondo quello
+    % che è settato dalle impostazioni.
     
-    %Titolo
-    sgtitle(sprintf('Risultato immagine %s\n%.1f%% selected',...
-        filename,selected_pixels_ratio));
+    [~, selected_pixels_ratio] = is_reliable(mask,IMG); % mi tornerà utile :)
+    
+    if show_resume == true
         
-    % Visualizzazione patterns prelevati
-    subplot(231); 
-    imagesc(IMG_RGB); axis image; colormap gray; hold on;
+        % --- Figure 1: riassuntazzo
+        figure();
+        
+        %Titolo
+        sgtitle(sprintf('Risultato immagine %s (rs)\n%.1f%% selected',...
+            filename,selected_pixels_ratio));
+        
+        % Visualizzazione patterns prelevati
+        subplot(231);
+        imagesc(IMG_RGB); axis image; colormap gray; hold on;
         if kernel_type == "GABOR"
             title('Immagine originale');
         else
-            title('Patterns prelevati');
-            rectangle('position',[1,1,kernel_dim,kernel_dim],'EdgeColor','r'); % pattern1
-            rectangle('position',[2,2,kernel_dim,kernel_dim],'EdgeColor','g'); % pattern2
-            rectangle('position',[IMG_x-kernel_dim+1,IMG_y-kernel_dim+1,kernel_dim,kernel_dim],'EdgeColor','b'); %pattern3
-            rectangle('position',[IMG_x-kernel_dim,IMG_y-kernel_dim,kernel_dim,kernel_dim],'EdgeColor','c'); %pattern4
-            rectangle('position',[1,IMG_y-kernel_dim+1,kernel_dim,kernel_dim],'EdgeColor','m'); %pattern5
-            rectangle('position',[2,IMG_y-kernel_dim+1,kernel_dim,kernel_dim],'EdgeColor','k'); %pattern6
+            title('Img originale');
+            
+            for krn=1:kernel_amount
+                position = kern_pos{krn};
+                pat_x = position(1);
+                pat_y = position(2);
+                rectangle('position',[pat_x,pat_y,kernel_dim,kernel_dim],'EdgeColor','r');
+            end
             hold off
         end
-    
-    if show_resume_choice == false
-        %Visualizziamo la xcorr risultante
-        corr_img = subplot(232);
-        imagesc(xcorr); axis image; colormap(corr_img,jet);
-        if kernel_type == "GABOR"
-           title('Gabor mediato');
+        
+        if show_resume_choice == false
+            %Visualizziamo la xcorr risultante
+            corr_img = subplot(232);
+            imagesc(xcorr); axis image; colormap(corr_img,jet);
+            if kernel_type == "GABOR"
+                title('Gabor mediato');
+            else
+                title('Mappa di cross-correlazione');
+            end
+            
+            %Visualizziamo la maschera raw
+            subplot(233);
+            imagesc(mask_raw); axis image;
+            title('Maschera raw');
+            
+            %Visualizziamo maschera rifinita
+            subplot(234);
+            imagesc(mask); axis image;
+            title('Maschera strel-ata');
+            
+            %Visualizziamo risultato
+            subplot(224);
+            imshowpair(IMG,IMG_masked,'montage')
+            title(sprintf('Risultato\n[%.2f%% - Kernel: %s,%d]',...
+                selected_pixels_ratio,kernel_type,kernel_dim));
+            
         else
-            title('Mappa di cross-correlazione');
+            % Visualizziamo le due maschere alternative
+            subplot(232);
+            imagesc(mask_corr); axis image;
+            title('Maschera CORR strel-ata');
+            subplot(233);
+            imagesc(mask_cont); axis image;
+            title('Maschera CONT strel-ata');
+            
+            %Visualizziamo risultato
+            subplot(212);
+            imshowpair(IMG,IMG_masked,'montage')
+            title(sprintf('Scelta: %s',kernel_type));
+            
         end
-
-        %Visualizziamo la maschera raw
-        subplot(233);
-        imagesc(mask_raw); axis image;
-        title('Maschera raw');
-
-        %Visualizziamo maschera rifinita
-        subplot(234);
-        imagesc(mask); axis image; 
-        title('Maschera strel-ata');
-
-        %Visualizziamo risultato
-        subplot(224);
-        imshowpair(IMG,IMG_masked,'montage')
-        title(sprintf('Risultato\n[%.2f%% - Kernel: %s,%d]',...
-              selected_pixels_ratio,kernel_type,kernel_dim));
-
-    else
-        % Visualizziamo le due maschere alternative
-       subplot(232);
-        imagesc(mask_corr); axis image; 
-        title('Maschera CORR strel-ata');      
-       subplot(233);
-        imagesc(mask_cont); axis image; 
-        title('Maschera CONT strel-ata');
         
-        %Visualizziamo risultato
-        subplot(212);
-        imshowpair(IMG,IMG_masked,'montage')
-        title(sprintf('Scelta: %s',kernel_type));
-
-    end
-      
         
-    % Salvataggio
-    resname =sprintf('results\\%s ',filename);
-    saveas(gcf, resname,'png');
-    
-    if analyze_just_one == false
-        close all;
+        % Salvataggio
+        resname =sprintf('results\\%s-RS',filename);
+        saveas(gcf, resname,'png');
+        
+        if analyze_just_one == false
+            close all;
+        end
     end
- end
     
-
- % ---- Figure 2 - risultato
- if show_result == true
-     
-    f=figure();
-    subplot(121);
-    imshow(IMG_RGB);
-    title('Immagine originale');
     
-    subplot(122);
-    imshow(IMG_masked);
-    title('Maschera');
-    
-   
-   sgtitle(sprintf('Risultato immagine %s\n\nT = %.3f\n%.1f%% selected\nTipo di analisi: %s',...
-       filename,T,selected_pixels_ratio,kernel_type));
-  
-    saveas(gcf, sprintf('results\\%s',filename),'png');
-    
-    if analyze_just_one == false
-        close(f);
+    % ---- Figure 2 - risultato
+    if show_result == true
+        
+        f=figure();
+        subplot(121);
+        imshow(IMG_RGB);
+        title('Immagine originale');
+        
+        subplot(122);
+        imshow(IMG_masked);
+        title('Maschera');
+        
+        
+        sgtitle(sprintf('Risultato immagine %s RS\n\nT = %.3f\n%.1f%% selected\nTipo di analisi: %s',...
+            filename,T,selected_pixels_ratio,kernel_type));
+        
+        saveas(gcf, sprintf('results\\%s-RS',filename),'png');
+        
+        if analyze_just_one == false
+            close(f);
+        end
     end
- end
-   
-
+    
+    
 end
+
+
+
